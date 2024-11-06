bd545d9f97ba47db1313580ab71d09ff6a1dbd8f
vijay <vijayvijayendra34@gmail.com>
2024-11-06 10:19:46 +0530

    first comment

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const userRouter = require('./routes/userRoutes');
 
 const app = express();
 
+////////////////////////////////////////////////////////////////////////
+
 // 1) MIDDLEWARES
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
